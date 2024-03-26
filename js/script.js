7e58fec745d07e476fab1376fea71753d30dbc23
ProgrammistAnthony <programmistanton@gmail.com>
2024-03-26 14:38:14 +0300

    new

--- js/script.js
+++ js/script.js
@@ -9,24 +9,18 @@ data.forEach((e) => {
     card.innerHTML = `
     <p class="emoji__smile">${e.symbol}</p>
     <h3 class="emoji__name">${e.title}</h3>
-    <p class="emoji__about">${e.keywords}</p>`;
+    <p class="emoji__about">${[...new Set(e.keywords.split(" "))].join(" ")}</p>`;
     divEmoji.append(card);
-    
-//     divEmoji.innerHTML += 
-    // <div class="emoji__card">
-    //     <p class="emoji__smile">${e.symbol}</p>
-    //     <h3 class="emoji__name">${e.title}</h3>
-    //     <p class="emoji__about">${e.keywords}</p>
-    // </div>`;
 });
 
 let inputHandler = (evt) => {
     let value = evt.target.value;
-    const findName = data.filter((el) => el.title.toLowerCase().includes(value.toLowerCase().trim()));
-    const findAbout = data.filter((el) => el.keywords.toLowerCase().includes(value.toLowerCase().trim()));
-    const result = Array.from(new Set([...findName, ...findAbout]));
+    const findName = data.filter((el) => el.title.toLowerCase().includes(value.toLowerCase()).trim());
+    const findAbout = data.filter((el) => el.keywords.toLowerCase().includes(value.toLowerCase()).trim());
+    const result = [...new Set([...findName, ...findAbout])];
     buttonHandler(result);
-};
+};  
+
 
 input.addEventListener('input', inputHandler);
 
@@ -39,14 +33,7 @@ function buttonHandler(data) {
     card.innerHTML = `
     <p class="emoji__smile">${e.symbol}</p>
     <h3 class="emoji__name">${e.title}</h3>
-    <p class="emoji__about">${e.keywords}</p>`;
+    <p class="emoji__about">${[...new Set(e.keywords.split(" "))].join(" ")}</p>`;
     divEmoji.append(card);
-
-        // divEmoji.innerHTML += `
-        // <div class="emoji__card">
-        //     <p class="emoji__smile">${e.symbol}</p>
-        //     <h3 class="emoji__name">${e.title}</h3>
-        //     <p class="emoji__about">${e.keywords}</p>
-        // </div>`;
     })
     };
